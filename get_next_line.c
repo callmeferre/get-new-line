94a7bbd113758be647431b64d59db471a3781328
callmeferre <callmeferre@gmail.com>
2021-08-25 17:11:50 +0200

    WIP: extra chars on last buf

--- get_next_line.c
+++ get_next_line.c
@@ -21,21 +21,10 @@ void	ft_fdlinedel(char	**str, const int fd)
 	}
 }
 
-char	*addline(char **str, const int fd)
+char	*doline(int len, char **str, const int fd, char *line)
 {
-	int		len;
 	char	*tmp;
-	char	*line;
 
-	len = 0;
-	line = NULL;
-	if (str[fd] == '\0' || str[fd][len] == '\0')
-	{
-		ft_fdlinedel(str, fd);
-		return (NULL);
-	}	
-	while (str[fd][len] != '\n' && str[fd][len] != '\0')
-		len++;
 	if (str[fd][len] == '\n')
 	{
 		line = ft_substr(str[fd], 0, len + 1);
@@ -51,27 +40,61 @@ char	*addline(char **str, const int fd)
 	return (line);
 }
 
+char	*addline(char **str, const int fd)
+{
+	int		len;
+	char	*line;
+
+	len = 0;
+	line = NULL;
+	if (str[fd] == '\0' || str[fd][len] == '\0')
+	{
+		ft_fdlinedel(str, fd);
+		return (NULL);
+	}	
+	while (str[fd][len] != '\n' && str[fd][len] != '\0')
+		len++;
+	return (doline(len, str, fd, line));
+}
+
 char	*get_next_line(const int fd)
 {
 	static char	*str[FILE_N];
 	char		buff[BUFFER_SIZE + 1];
 	char		*tmp;
+	int			chars;
 
 	if (fd < 0 || read(fd, buff, 0) == -1)
 		return (NULL);
-		
-	while (read(fd, buff, BUFFER_SIZE))
+	chars = read(fd, buff, BUFFER_SIZE);
+	while (chars)
 	{	
 		buff[BUFFER_SIZE] = '\0';
 		if (!str[fd])
-			str[fd] = ft_calloc(sizeof(char), BUFFER_SIZE);
+			str[fd] = ft_strdup(buff);
+		else
+		{
+			if (chars != BUFFER_SIZE)
+			{
+				tmp = ft_strjoin(str[fd], ft_substr(buff, buff[0], chars));
+				printf("%s\n", ft_substr(buff, buff[0], chars));
+			}
+			else
+			{
+				tmp = ft_strjoin(str[fd], buff);
+				//printf("%s\n", tmp);
 
-		tmp = ft_strjoin(str[fd], buff);
-		free(str[fd]);
-		str[fd] = tmp;
-		if (ft_strchr(buff, '\n'))
-				break ;
+			}
+			
+			free(str[fd]);
+			str[fd] = tmp;
+		}
+		
+		if (ft_strchr(buff, '\n') || chars == 0)
+			break ;
+		
+		chars = read(fd, buff, BUFFER_SIZE);
+		
 	}
-	printf("linea: %s\n", str[fd]);
 	return (addline(str, fd));
 }
